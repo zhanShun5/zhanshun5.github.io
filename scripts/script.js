4ce34bf8b576021e00bedc6cbd4e5d52ac213bd1
Butter <zhanShun5@users.noreply.github.com>
2025-03-22 14:21:45 +0100

    Perfomance imrpovement
    I didn't optimize the images, oops.
    Now I do.

--- scripts/script.js
+++ scripts/script.js
@@ -88,25 +88,3 @@ document.querySelectorAll('CCVideo').addEventListener('click', function() {
       this.msRequestFullscreen();
     }
 });
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    const fadeElements = document.querySelectorAll('.fade-in');
-    
-    function checkVisibility() {
-      fadeElements.forEach(element => {
-        const elementTop = element.getBoundingClientRect().top;
-        const elementVisible = 150; // How many pixels from the viewport edge to trigger
-        
-        if (elementTop < window.innerHeight - elementVisible) {
-          element.classList.add("visible");
-        }
-      });
-    }
-    
-    // Check visibility on load
-    checkVisibility();
-    
-    // Check visibility on scroll
-    window.addEventListener("scroll", checkVisibility);
-  });
