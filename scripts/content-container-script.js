22fefa65b49ff2db9344f549164d2c1a394feefc
Butter <zhanShun5@users.noreply.github.com>
2025-03-25 13:42:27 +0100

    auto scroll on refresh & no hash on nav press
    Prevent automatic browser scroll on refresh.
    
    On nav link press, no hash is added

--- scripts/content-container-script.js
+++ scripts/content-container-script.js
@@ -64,3 +64,6 @@ window.addEventListener('pageshow', (event) => {
         initContentContainerScript();
     }
 });
+
+
+ 
--- scripts/script.js
+++ scripts/script.js
@@ -143,4 +143,3 @@ scrollToTopBtn.addEventListener("click", scrollToTop);
 //     }
 // });
 
-
